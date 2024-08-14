67893d5e777aa9b83d3de43a6aaf10c8b4ca8930
AiLoveX <fantasyloaded.exe@gmail.com>
2024-08-14 15:28:41 +0300

    Change class FirstGit add sout

--- src/main/java/by/javaguru/FirstGit.java
+++ src/main/java/by/javaguru/FirstGit.java
@@ -2,7 +2,7 @@ package by.javaguru;
 
 public class FirstGit {
     public static void main(String[] args) {
-        System.out.println("GIT");
+        System.out.println("GIT Develop");
         System.out.println("LEARNING!");
     }
 }
