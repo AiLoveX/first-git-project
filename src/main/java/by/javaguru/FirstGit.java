f2e434895676ea5334b6071170567b65e1e631ef
AiLoveX <fantasyloaded.exe@gmail.com>
2024-08-14 15:30:29 +0300

    Change sout

--- src/main/java/by/javaguru/FirstGit.java
+++ src/main/java/by/javaguru/FirstGit.java
@@ -4,5 +4,6 @@ public class FirstGit {
     public static void main(String[] args) {
         System.out.println("GIT Develop");
         System.out.println("LEARNING!");
+        System.out.println("!!!!!!");
     }
 }
