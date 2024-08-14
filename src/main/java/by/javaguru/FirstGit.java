6c1fec55fc8b9392814cd79fc22ee5f0f8863a96
AiLoveX <118376587+AiLoveX@users.noreply.github.com>
2024-08-14 15:19:55 +0300

    Update FirstGit.java
    Add ! to sout FirstGit

--- src/main/java/by/javaguru/FirstGit.java
+++ src/main/java/by/javaguru/FirstGit.java
@@ -3,6 +3,6 @@ package by.javaguru;
 public class FirstGit {
     public static void main(String[] args) {
         System.out.println("GIT");
-        System.out.println("LEARNING");
+        System.out.println("LEARNING!");
     }
 }
